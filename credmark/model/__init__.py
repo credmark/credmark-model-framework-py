26bb58e251f2901089aee91019d16957df80d8a0
Yang Ye <214410+leafyoung@users.noreply.github.com>
2022-02-14 12:05:00 +0800

    - Make credmark.model module callable so credmark.model() can be imported and used as a decorator

--- credmark/model/__init__.py
+++ credmark/model/__init__.py
@@ -1,3 +1,90 @@
-from .base import Model
 from .context import ModelContext
-from .manifest_v1 import manifest_v1
+from .errors import ModelRunError
+
+import sys
+import types
+import inspect
+from typing import Union
+
+from .base import Model
+from .errors import WrongModelMethodSignature
+
+
+class CreateModel(types.ModuleType):
+    def __init__(self):
+        types.ModuleType.__init__(self, __name__)
+        # or super().__init__(__name__) for Python 3
+        self.__dict__.update(sys.modules[__name__].__dict__)
+
+    def __call__(self,
+                 slug: str,
+                 version=Union[str, None],
+                 display_name=Union[str, None],
+                 description=Union[str, None]):
+
+        def wrapper(cls_in):
+            def is_parent(child, parent):
+                found = parent in child.__bases__
+                return found or True in [is_parent(pp, parent) for pp in child.__bases__]
+
+            if not is_parent(cls_in, Model):
+                cls = type(cls_in.__name__, (Model, *cls_in.__bases__), dict(cls_in.__dict__))
+            else:
+                cls = cls_in
+
+            attr_value = {
+                'credmarkModelManifest': 'v1',
+                'model': {
+                    'slug': slug,
+                    'version': version,
+                    'display_name': display_name,
+                    'description': description,
+                    'class': cls.__dict__['__module__'] + '.' + cls.__name__
+                }
+            }
+
+            attr_name = '__manifest'
+            setattr(cls, attr_name, attr_value)
+
+            # Checks for the class
+            # 1. it need to be inherited from Model class
+            assert is_parent(cls, Model)
+
+            # 2. has abstract functions as defined in the Model class
+            method_list = [method for method in dir(Model)
+                           if method.startswith('__') is False and
+                           '__isabstractmethod__' in Model.__dict__[method].__dict__ and
+                           Model.__dict__[method].__isabstractmethod__]
+
+            for method in method_list:
+                found_this_method = False
+                for mro_cls in cls.__mro__:
+                    if method not in mro_cls.__dict__:
+                        continue
+
+                    found_this_method = True
+                    try:
+                        assert inspect.signature(Model.__dict__[method]) == inspect.signature(
+                            mro_cls.__dict__[method])
+                        break
+                    except:
+                        if mro_cls == cls:
+                            msg = (f'Model {cls.__name__} does not define method '
+                                   f'{method}{inspect.signature(cls.__dict__[method])} with '
+                                   f'signature {inspect.signature(Model.__dict__[method])}')
+                        else:
+                            msg = (f'Model\'s parent {mro_cls.__name__} does not define '
+                                   f'method {method}{inspect.signature(mro_cls.__dict__[method])} with '
+                                   f'signature {inspect.signature(Model.__dict__[method])}')
+                        raise WrongModelMethodSignature(msg)
+
+                if not found_this_method:
+                    raise WrongModelMethodSignature(
+                        f'Model {cls.__name__} misses a method {method}() with signature {inspect.signature(Model.__dict__[method])}')
+
+            return cls
+
+        return wrapper
+
+
+sys.modules[__name__] = CreateModel()
