334b758375d8e5ed067200a376679b94c3e53663
Neil Zumwalde <neilzumwalde@gmail.com>
2022-02-18 14:45:30 -0600

    contract by address example

--- credmark/model/contract_helper.py
+++ credmark/model/contract_helper.py
@@ -15,16 +15,37 @@ class ContractHelper:
                       protocol: str = None,
                       product: str = None,
                       abi: dict = None,
-                      ):
-        if address is not None:
-            contract = self.context.run_model('contract.metadata', {"contractAddress": address})
-        if name is not None:
+                      tags: dict = None,):
+        if name is None and address is None and abi is None:
+            raise Exception
 
-            conts = self.context.run_model('contract.metadata', {"contractName": name})
-            contracts = []
-            for cont in conts:
-                contracts.append(self.context.web3.eth.contract(
-                    address=self.context.web3.toChecksumAddress(cont['ADDRESS']),
-                    abi=cont['ABI']
-                ))
-            return contracts
+            # This means we can end up with different KINDS of contracts together. probably no bueno
+            # we could do it if we could return a contract that is a subclass of web3.contract.Contract
+            # but I don't understand how to do that with a web3 context
+
+        contracts = []
+        q = {}
+
+        if address is not None:
+            q["contractAddress"] = address
+        if name is not None:
+            q["contractName"] = name
+        if protocol is not None:
+            q["protocol"] = protocol
+        if product is not None:
+            q["product"] = product
+        if tags is not None:
+            q["tags"] = tags
+        if abi is not None:
+            q["abi"] = abi
+
+        contract_q_results = self.context.run_model('contract.metadata', q)
+
+        for contract in contract_q_results:
+
+            contracts.append(self.context.web3.eth.contract(
+                address=self.context.web3.toChecksumAddress(contract['ADDRESS']),
+                abi=contract['ABI']
+            ))
+
+        return contracts
