163a3c46f0c29a916b481c2239343d8bfd9ce9c2
iwyrkore <iwyrkore@gmail.com>
2022-02-21 18:37:28 -0500

    Validate columns as lowercase in ledger.

--- credmark/model/ledger/ledger.py
+++ credmark/model/ledger/ledger.py
@@ -19,11 +19,13 @@ class Ledger:
     def __init__(self, context):
         self.context = context
 
-    def _validate_columns(self, model_slug: str, columns: List[str], ledger_object_type: type[LedgerTable]):
+    def _validate_columns(self, model_slug: str,
+                          columns: List[str],
+                          ledger_object_type: type[LedgerTable]):
         column_set = ledger_object_type.columns()
 
         for column in columns:
-            if column.upper() not in column_set:
+            if column.lower() not in column_set:
                 raise InvalidColumnException(
                     model_slug,
                     column, list(column_set), "invalid column name")
