c3cfcef34377cd416a951094c20273c194840325
Neil Zumwalde <neilzumwalde@gmail.com>
2022-03-01 17:35:45 -0600

    blocknumber fails if you go higher than the model context

--- credmark/types/data/block_number.py
+++ credmark/types/data/block_number.py
@@ -6,6 +6,23 @@ from typing import (
     Union,
 )
 
+class BlockNumberOutOfRangeException(Exception):
+    def __init__(self, block_number: int, max_block_number: int, message: str):
+        self.block_number = block_number
+        self.max_block_number = max_block_number
+        super().__init__(message)
+
+    def __str__(self):
+        return f'BlockNumber {self.block_number} is out of maximum range: {self.max_block_number}'
+
+class InvalidBlockNumberException(Exception):
+    def __init__(self, block_number: int, message: str):
+        self.block_number = block_number
+        super().__init__(message)
+
+    def __str__(self):
+        return f'BlockNumber {self.block_number} is invalid'
+
 class BlockNumber(int):
     def __new__(cls,
                 number: Union[int, None] = None,
@@ -13,16 +30,22 @@ class BlockNumber(int):
                 timestamp: Union[Timestamp, None] = None,  # pylint: disable=unused-argument
                 sample_timestamp: Union[Timestamp, None] = None):
         if number is None:
-            if sample_timestamp is not None:
-                if context is not None:
-                    get_blocknumber_result = context.run_model(
-                        'rpc.get-blocknumber', {"timestamp": sample_timestamp})
-                    return BlockNumber(number=get_blocknumber_result['blockNumber'],
-                                       context=context,
-                                       timestamp=get_blocknumber_result['blockTimestamp'],
-                                       sample_timestamp=sample_timestamp)
-        else:
-            return super().__new__(BlockNumber, number)
+            if sample_timestamp is not None and context is not None:
+                get_blocknumber_result = context.run_model(
+                    'rpc.get-blocknumber', {"timestamp": sample_timestamp})
+                return BlockNumber(number=get_blocknumber_result['blockNumber'],
+                                    context=context,
+                                    timestamp=get_blocknumber_result['blockTimestamp'],
+                                    sample_timestamp=sample_timestamp)
+            else:
+                raise InvalidBlockNumberException(number, "Invalid Block Number")
+        try:
+            max_block_number = context.block_number
+        except:
+            max_block_number = None
+        if max_block_number is not None and number > max_block_number:
+            raise BlockNumberOutOfRangeException(number, max_block_number, "Block Number out of Range")
+        return super().__new__(BlockNumber, number)
 
     def __init__(self,
                  number: Union[int, None] = None,  # pylint: disable=unused-argument
