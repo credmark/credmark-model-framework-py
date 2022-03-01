15cb769be021b3d317453e9bbcce481313362bea
Neil Zumwalde <neilzumwalde@gmail.com>
2022-03-01 14:27:43 -0600

    historical util : block

--- credmark/utils/historical_util.py
+++ credmark/utils/historical_util.py
@@ -74,59 +74,41 @@ class HistoricalUtil:
                 "end": end_timestamp,
                 "interval": interval_timestamp
             }
-            print(series_input)
-            print(self.context.run_model('series.time-start-end-interval', series_input))
             return self.context.run_model('series.time-start-end-interval', series_input)
 
     def run_model_historical_blocks(self,
                              model_slug: str,
-                             window: str,
+                             window: int,
+                             interval: int,
                              model_input: dict = {},
-                             interval: str = None,
-                             end_timestamp: int = None,
-                             snap_clock: Union[str, None] = 'interval',
+                             end_block: Union[int, None] = None,
+                             snap_block: Union[int, None] = None,
                              model_version: Union[str, None] = None) -> dict:
-        (w_k, w_v) = self.parse_timerangestr(window)
-        window_timestamp = self.range_timestamp(w_k, w_v)
-        if interval is not None:
-            (i_k, i_v) = self.parse_timerangestr(interval)
-            interval_timestamp = self.range_timestamp(i_k, i_v)
-            if snap_clock == 'interval':
-                snap_clock = interval_timestamp
-        else:
-            interval_timestamp = self.range_timestamp(w_k, 1)
-            if snap_clock == 'interval':
-                snap_clock = w_k
 
-        if snap_clock is None and end_timestamp is None:
+        if snap_block is None and end_block is None:
             series_input = {
                 "modelSlug": model_slug, 
                 "modelInput": model_input,
                 "modelVersion": model_version,
-                "window": window_timestamp,
-                "interval": interval_timestamp
+                "window": window,
+                "interval": interval
             }
-            return self.context.run_model('series.time-window-interval', series_input)
+            return self.context.run_model('series.block-window-interval', series_input)
         else:
-            
-            if end_timestamp is None:
-                end_timestamp = self.context.block_number.timestamp
-            if snap_clock is not None:
-                (s_k, s_v) = self.parse_timerangestr(snap_clock)
-                snap_sec = self.range_timestamp(s_k, s_v)
-                end_timestamp = end_timestamp - (end_timestamp % snap_sec)
+            if end_block is None:
+                end_block = self.context.block_number
+            if snap_block is not None:
+                end_block = end_block - (end_block % snap_block)
 
             series_input = {
                 "modelSlug": model_slug, 
                 "modelInput": model_input,
                 "modelVersion": model_version,
-                "start": end_timestamp - window_timestamp,
-                "end": end_timestamp,
-                "interval": interval_timestamp
+                "start": end_block - window,
+                "end": end_block,
+                "interval": interval
             }
-            print(series_input)
-            print(self.context.run_model('series.time-start-end-interval', series_input))
-            return self.context.run_model('series.time-start-end-interval', series_input)
+            return self.context.run_model('series.block-start-end-interval', series_input)
 
     
     def parse_timerangestr(self, time_str: str):
