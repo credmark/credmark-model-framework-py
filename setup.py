bd06e0eed3d988ad0cd6fae23fc4fa106c1f1611
iwyrkore <iwyrkore@gmail.com>
2022-03-24 15:54:19 -0400

    Version 0.7.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='credmark-model-framework',
-    version='0.7.2',
+    version='0.7.3',
     description='Credmark model development framework',
     long_description=readme + '\n\n' + history,
     author='Credmark',
