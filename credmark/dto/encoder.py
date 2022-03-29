9eb5d09042dd2b977fe1b8d4087ca3b0b065fb17
kunlun <214410+leafyoung@users.noreply.github.com>
2022-03-29 14:35:47 +0800

    add parameter to json.dump

--- credmark/dto/encoder.py
+++ credmark/dto/encoder.py
@@ -17,9 +17,9 @@ class PydanticJSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 
-def json_dump(obj, fp, post_proc=lambda x:x, *a, **b):  # pylint: disable=invalid-name
+def json_dump(obj, fp, post_proc=lambda x: x, **b):  # pylint: disable=invalid-name
     """Dump an obj that may contain embedded DTOs to json"""
-    return post_proc(json.dump(obj, fp, cls=PydanticJSONEncoder, *a, **b))
+    return post_proc(json.dump(obj, fp, cls=PydanticJSONEncoder, **b))
 
 
 def json_dumps(obj):
