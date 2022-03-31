86cf28a298b3b297b4522fe2bcc7088051c566d5
iwyrkore <iwyrkore@gmail.com>
2022-03-31 15:59:02 -0400

    Added TokenWei to cmf.types module import

--- credmark/cmf/types/__init__.py
+++ credmark/cmf/types/__init__.py
@@ -1,10 +1,11 @@
 # pylint: disable=locally-disabled, unused-import
 
+from .account import Account, Accounts
 from .address import Address, NULL_ADDRESS
 from .block_number import BlockNumber
-from .account import Account, Accounts
 from .contract import Contract, Contracts
-from .token import Token, Tokens, NativeToken
 from .portfolio import Portfolio
 from .position import Position, NativePosition, TokenPosition
 from .price import Price, TokenPairPrice
+from .token_wei import TokenWei
+from .token import Token, Tokens, NativeToken
