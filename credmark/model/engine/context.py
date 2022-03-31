c6f6c2d23db22d5b1ebad9ee91f897a8bb1b22d6
kunlun <214410+leafyoung@users.noreply.github.com>
2022-03-31 15:34:55 +0800

    change the order of display

--- credmark/model/engine/context.py
+++ credmark/model/engine/context.py
@@ -334,13 +334,15 @@ class EngineModelContext(ModelContext):
                 else:
                     if self.dev_mode:
                         self.logger.exception(
-                            f'Exception running model {slug}({input}) with {err} on '
+                            f'Exception running model {slug}({input}) on '
                             f'block {context.block_number}/'
-                            f'{context.block_number.timestamp_datetime:%Y-%m-%d %H:%M%S}')
+                            f'{context.block_number.timestamp_datetime:%Y-%m-%d %H:%M%S} '
+                            f'with {err}')
                     err = ModelRunError(
-                        f'Exception running model {slug}({input}) with {err} on '
+                        f'Exception running model {slug}({input}) on '
                         f'block {context.block_number}/'
-                        f'{context.block_number.timestamp_datetime:%Y-%m-%d %H:%M%S}')
+                        f'{context.block_number.timestamp_datetime:%Y-%m-%d %H:%M%S} '
+                        f'with {err}')
                     trace = traceback.format_exc(limit=30)
                 # We add the model just run (or validated input for) to stack
                 err.data.stack.insert(0,
