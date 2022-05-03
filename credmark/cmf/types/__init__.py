9e2255c8d103bf4e26a4faa7011a9b4f953a13c7
iwyrkore <iwyrkore@gmail.com>
2022-05-03 13:15:45 -0400

    Added LedgerContract to credmark.cmf.types

--- credmark/cmf/types/__init__.py
+++ credmark/cmf/types/__init__.py
@@ -4,6 +4,7 @@ from .account import Account, Accounts
 from .address import Address, NULL_ADDRESS
 from .block_number import BlockNumber
 from .contract import Contract, Contracts
+from .ledger import ContractLedger
 from .portfolio import Portfolio
 from .position import Position, NativePosition, TokenPosition
 from .price import Price, TokenPairPrice, PriceList
