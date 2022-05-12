5043bd8e90bb0efffe1cf80da3c2683bc9863d7a
DHG18 <dahg18@gmail.com>
2022-05-12 12:23:13 -0400

    added get_value functions for both portfolio and position classes

--- credmark/cmf/types/portfolio.py
+++ credmark/cmf/types/portfolio.py
@@ -12,6 +12,9 @@ class Portfolio(IterableListGenericDTO[Position]):
     positions: List[Position] = DTOField(default=[], description='List of positions')
     _iterator: str = PrivateAttr('positions')
 
+    def get_value(self):
+        return sum([pos.get_value() for pos in self.positions])
+
     class Config:
         schema_extra: dict = {
             'examples': [{'positions': [exp]} for exp in Position.Config.schema_extra['examples']]
--- credmark/cmf/types/position.py
+++ credmark/cmf/types/position.py
@@ -1,10 +1,17 @@
 from credmark.dto import DTO, DTOField, cross_examples
 from .token import Token, NativeToken
+from credmark.cmf.model import ModelContext
 
 
 class Position(DTO):
     amount: float = DTOField(0.0, description='Quantity of token held')
     asset: Token
+
+    def get_value(self):
+        context = ModelContext.current_context()
+        price = context.run_model('token.price', input=self.asset,
+                                  block_number=context.block_number)['price']
+        return price * self.amount
 
     class Config:
         schema_extra = {
