c63e3c75628014a9fc67e6673bb7affe7d96dee6
iwyrkore <iwyrkore@gmail.com>
2022-06-23 15:20:05 -0400

    Updated credmark-dev docs.

--- credmark/cmf/credmark_dev.py
+++ credmark/cmf/credmark_dev.py
@@ -553,7 +553,7 @@ def run_model(args):  # pylint: disable=too-many-statements,too-many-branches,to
             if format_json:
                 print(json_dumps(result, indent=4).replace('\\n', '\n').replace('\\"', '\''))
             elif args['output']:
-                logger.info(f"saving model results to {args['output']}")
+                logger.info(f"Saving model results to {args['output']}")
                 with open(args['output'], 'w') as fp:
                     json.dump(result, fp, indent=4)
             else:
