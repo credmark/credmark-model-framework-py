c82d1317b7b672edb040d9e0642d726086e128d1
iwyrkore <iwyrkore@gmail.com>
2022-06-29 15:07:29 -0400

    Updated token classes docs.

--- credmark/cmf/types/token.py
+++ credmark/cmf/types/token.py
@@ -47,8 +47,7 @@ def get_token_from_configuration(
 
 class Token(Contract):
     """
-    Token represents a fungible Token that conforms to ERC20
-    standards
+    Fungible Token that conforms to ERC20 standards.
     """
 
     class TokenMetadata(Contract.ContractMetaData):
@@ -204,10 +203,16 @@ class Token(Contract):
 
 
 class TokenInfo(Token):
+    """
+    Subclass of Token containing its related metadata.
+    """
     meta: Token.TokenMetadata
 
 
 class NativeToken(Token):
+    """
+    Native token for a chain, such as "ETH" or "MATIC".
+    """
 
     def __init__(self, **kwargs) -> None:
         context = credmark.cmf.model.ModelContext.current_context()
@@ -252,12 +257,22 @@ class NativeToken(Token):
 
 
 class NonFungibleToken(Contract):
+    """
+    Non-fungible token.
+
+    `This class is not yet implemented`
+    """
+
     def __init__(self, **data):
         super().__init__(**data)
         raise NotImplementedError()
 
 
 class Tokens(IterableListGenericDTO[Token]):
+    """
+    Iterable list of Token instances.
+    """
+
     tokens: List[Token] = DTOField(
         default=[], description="An iterable list of Token Objects")
     _iterator: str = PrivateAttr('tokens')
@@ -265,7 +280,7 @@ class Tokens(IterableListGenericDTO[Token]):
 
 class FiatCurrency(Account):
     """
-    This is DTO for Fiat Currency.
+    Fiat currency.
     """
 
     class FiatCurrencyMeta:
@@ -327,7 +342,7 @@ class FiatCurrency(Account):
 
 class Currency(Account):
     """
-    This is a converter for any Fungible Token and FiatCurrency.
+    Converter for any Fungible Token and FiatCurrency.
     It's used as inputs to price models.
 
     It can be constructed with a string (containing an address or symbol)
