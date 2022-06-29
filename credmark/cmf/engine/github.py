47e174709d960b21f7a272019bf4717ed1f5e2dc
iwyrkore <92745880+iwyrkore@users.noreply.github.com>
2022-06-28 22:27:05 -0400

    Added checking of latest version tag in github. (#125)

---
+++ credmark/cmf/engine/github.py
@@ -0,0 +1,22 @@
+import logging
+import requests
+
+logger = logging.getLogger(__name__)
+
+GITHUB_API_TAGS_URL = 'https://api.github.com/repos/credmark/credmark-model-framework-py/tags'
+
+
+def get_latest_cmf_version_tag():
+    tag = None
+
+    try:
+        response = requests.get(GITHUB_API_TAGS_URL)
+        response.raise_for_status()
+        tag_list = response.json()
+        if len(tag_list):
+            tag = tag_list[0]['name']
+    except Exception as err:
+        logger.error(
+            f'Error getting latest cmf version from {GITHUB_API_TAGS_URL}: {err}')
+
+    return tag
