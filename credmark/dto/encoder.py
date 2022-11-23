b79414543b65dffcaea254b3e407539ef51354a2
Yang Ye <kunlun@credmark.com>
2022-11-23 11:30:27 +0800

    add type

--- credmark/dto/encoder.py
+++ credmark/dto/encoder.py
@@ -14,13 +14,14 @@ class PydanticJSONEncoder(json.JSONEncoder):
       json.dump(result, cls=PydanticJSONEncoder)
     """
 
-    def encode(self, o):
+    def encode(self, o) -> str:
         # A top-level primitive DTO (IntDTO, StrDTO, FloatDTO)
         # is serialized as a dict.
         if isinstance(o, DTOTypesTuple):
             o = o.dict()
         return super().encode(o)
 
+    # pylint: disable=too-many-return-statements
     def default(self, o):
         # Primitive DTO types (IntDTO, StrDTO, FloatDTO) are
         # serialized as primitive types if not the top-level DTO.
@@ -41,11 +42,11 @@ class PydanticJSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 
-def json_dump(obj, fp, **json_dump_args):  # pylint: disable=invalid-name
+def json_dump(obj, fp, **json_dump_args) -> None:  # pylint: disable=invalid-name
     """Dump an o that may contain embedded DTOs to json"""
     return json.dump(obj, fp, cls=PydanticJSONEncoder, **json_dump_args)
 
 
-def json_dumps(obj, **json_dump_args):
+def json_dumps(obj, **json_dump_args) -> str:
     """Dump an obj that may contain embedded DTOs to json string"""
     return json.dumps(obj, cls=PydanticJSONEncoder, **json_dump_args)
