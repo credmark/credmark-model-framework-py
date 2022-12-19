ad2d8d4a4b2dbce7ea7717792b6b605cb7c1b0e0
Yang Ye <kunlun@credmark.com>
2022-12-19 22:36:29 +0800

    add error message for missing web3

--- credmark/cmf/engine/web3_registry.py
+++ credmark/cmf/engine/web3_registry.py
@@ -59,8 +59,11 @@ class Web3Registry:
         self.__chain_to_provider_url = chain_to_provider_url if \
             chain_to_provider_url is not None else {}
 
+    # pylint:disable=line-too-long
     def web3_for_chain_id(self, chain_id: int):
         url = self.__chain_to_provider_url.get(str(chain_id))
         if url is None:
-            raise Exception(f'No web3 provider url for chain id {chain_id}')
+            raise Exception(
+                f'No web3 provider url for chain id {chain_id}. '
+                f'In environment or .env file, set CREDMARK_WEB3_PROVIDERS or CREDMARK_WEB3_PROVIDER_CHAIN_ID_{chain_id}')
         return self.web3_for_provider_url(url, chain_id)
