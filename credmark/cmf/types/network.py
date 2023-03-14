4dd00449bb38b57b15dcd8aaeae065d5bd8f93ed
v1nvn <v1n@outlook.com>
2023-03-14 17:06:29 +0530

    Added more networks

--- credmark/cmf/types/network.py
+++ credmark/cmf/types/network.py
@@ -6,11 +6,13 @@ class Network(IntEnum):
     Mainnet = 1
     Ropsten = 3
     Rinkeby = 4
-    Optimism = 10
-    ArbitrumOne = 42161
     Görli = 5
+    Optimism = 10
     Kovan = 42
     BSC = 56
     BSCTestnet = 97
     xDai = 100
     Polygon = 137
+    Fantom = 250
+    ArbitrumOne = 42161
+    Avalanche = 43114
