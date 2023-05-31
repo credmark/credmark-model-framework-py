c26d9822ab764a3db474ed7e5317c70a4ad6eba1
kunlun <leafyoung@gmail.com>
2023-05-31 18:04:44 +0800

    add polygon tokens

--- credmark/cmf/types/data/fungible_token_data_polygon.py
+++ credmark/cmf/types/data/fungible_token_data_polygon.py
@@ -6,17 +6,41 @@ POLYGON_TOKENS = {
         'address': '0x0000000000000000000000000000000000001010',
         "is_native_token": True,
     },
+    'WMATIC': {
+        "symbol": "WMATIC",
+        "decimals": 18,
+        "name": "Wrapped Matic",
+        'address': '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+    },
     "WETH": {
         "symbol": "WETH",
         "decimals": 18,
         "name": "Wrapped Ether",
         'address': '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
     },
+    "WBTC": {
+        "symbol": "WBTC",
+        "decimals": 8,
+        "name": "(PoS) Wrapped BTC",
+        'address': '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6',
+    },
     "USDC": {
         "symbol": "USDC",
         "decimals": 6,
         "name": "USD Coin (PoS)",
         'address': '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+    },
+    "USDT": {
+        "symbol": "USDT",
+        "decimals": 6,
+        "name": "(PoS) Tether USD",
+        'address': '0xc2132D05D31c914a87C6611C10748AEb04B58e8F'
+    },
+    "DAI": {
+        "symbol": "DAI",
+        "decimals": 18,
+        "name": "(PoS) Dai Stablecoin",
+        'address': '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063'
     },
     "miMATIC": {
         "symbol": "miMATIC",
@@ -47,12 +71,6 @@ POLYGON_TOKENS = {
         "decimals": 18,
         "name": "ChainLink Token",
         'address': '0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39',
-    },
-    'WMATIC': {
-        "symbol": "WMATIC",
-        "decimals": 18,
-        "name": "Wrapped Matic",
-        'address': '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
     },
     'SUSHI': {
         "symbol": "SUSHI",
