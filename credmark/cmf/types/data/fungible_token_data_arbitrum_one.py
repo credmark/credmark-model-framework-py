4f69eff8f3be419abea00f6047b76715d97216bd
kunlun <leafyoung@gmail.com>
2023-06-03 12:14:06 +0800

    "Updated files with my changes"

--- credmark/cmf/types/data/fungible_token_data_arbitrum_one.py
+++ credmark/cmf/types/data/fungible_token_data_arbitrum_one.py
@@ -866,11 +866,17 @@ ARBITRUM_ONE_TOKENS = {
         "name": "Uniswap",
         "address": "0xfa7f8980b0f1e64a2062791cc3b0871572f1f7f0"
     },
-    "USDC": {
-        "symbol": "USDC",
+    "USDC.e": {
+        "symbol": "USDC.e",
         "decimals": 6,
         "name": "USD Coin (Arb1)",
         "address": "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8"
+    },
+    "USDC": {
+        "symbol": "USDC",
+        "decimals": 6,
+        "name": "USD Coin",
+        "address": "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
     },
     "USDD": {
         "symbol": "USDD",
